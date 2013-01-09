3ee8d09e203331252aa1110e4801ac3e1863a3d1
Andreas Bjärlestam <bjarlestam@gmail.com>
2013-01-09 13:45:34 +0100

    added tmux configuration

--- setup.sh
+++ setup.sh
@@ -3,4 +3,5 @@ ln -s ~/dotfiles/.bash_profile ~/.bash_profile
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/dotfiles/.gitignore ~/.gitignore
 ln -s ~/dotfiles/.hgrc ~/.hgrc
+ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 
