81e53e22a24f04a1a9e2ad553a54b409c882c3c5
Andreas Bjärlestam <bjarlestam@gmail.com>
2014-03-16 17:23:30 +0100

    added oh-my-zsh to setup.sh

--- setup.sh
+++ setup.sh
@@ -5,3 +5,9 @@ ln -s ~/dotfiles/.gitignore ~/.gitignore
 ln -s ~/dotfiles/.hgrc ~/.hgrc
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 
+
+if [ ! -d ~/.oh-my-zsh ]; then
+  git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+fi
+
+chsh -s `which zsh`
